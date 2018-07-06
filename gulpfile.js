48aaa9c627ea950b0633097f7d10634498a7401c
Rustam4457 <punk60826@gmail.com>
2018-07-07 02:14:24 +0300

    fonts, styles and scripts in gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ const sourcemaps = require('gulp-sourcemaps');
 const cache = require('gulp-cache');
 const csso = require('gulp-csso');
 const concat = require('gulp-concat');
+const rigger = require('gulp-rigger');
+
+
 
 gulp.task('serve', () =>{
     browserSync.init({
@@ -22,51 +25,59 @@ gulp.task('serve', () =>{
 });
 
 gulp.task('html', () =>{
-   return gulp.src('index.html')
-   .pipe(gulp.dest('build'))
+   return gulp.src('src/index.html')
+   .pipe(gulp.dest('build/'))
    .on('end', browserSync.reload);
 });    
 
-gulp.task('imagemin', function () {
-  return gulp.src('assets/img/*.png')
+gulp.task('imagemin',  ()=> {
+  return gulp.src('src/img/**/*.{jpg, png, svg}')
     .pipe(cache(imagemin({
       optimizationLevel: 7,
       progressive: true,
       interlaced: true
     })))
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest('build/img'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
+gulp.task('fonts', ()=> {
+    return gulp.src('src/fonts/**/*.*')
+    .pipe(gulp.dest('build/fonts'))
+});
 gulp.task('less', () => {
-  return gulp.src('assets/less/*.less')
+  return gulp.src('src/less/style.less')
     .pipe(sourcemaps.init() )
     .pipe(less())
     .pipe(plumber() )
     .pipe(csso())
-    .pipe(concat('build/common.css'))
-		.pipe(autoprefixer({
-		    browsers: ['last 10 versions'],
-		    cascade: false
-		}))
-		.on("error", notify.onError({
+    .pipe(autoprefixer({
+        browsers: ['last 10 versions'],
+        cascade: false
+    }))
+    .on("error", notify.onError({
         title: "stytle"
-		}))
+    }))
    .pipe(sourcemaps.write() )
-    .pipe(gulp.dest('build/'))
+    .pipe(gulp.dest('build/css'))
     .pipe( browserSync.reload({
       stream: true
     }));
 });
-
+gulp.task('script', function() {
+  return gulp.src('src/js/**/*.*')
+  .pipe(rigger())
+  .pipe(gulp.dest('build/js'));
+});
 gulp.task('watch', () =>{
-   gulp.watch('index.html', gulp.series('html')),
-   gulp.watch('assets/**/*.{jpg, png, svg}', gulp.series('imagemin')),
-   gulp.watch('assets/**/*.{less, css}', gulp.series('less'))
+   gulp.watch('src/index.html', gulp.series('html')),
+   gulp.watch('src/img/**/*.{jpg, png, svg}', gulp.series('imagemin')),
+   gulp.watch('src/fonts/**/*.*', gulp.series('fonts')),
+   gulp.watch('src/less/style.less', gulp.series('less')),
+   gulp.watch('src/js/script.js', gulp.series('script'))
 });
 
 gulp.task('default', gulp.series(
-gulp.parallel('html', 'less', 'imagemin'),
+gulp.parallel('html', 'less', 'imagemin', 'fonts', 'script'),
 gulp.parallel('watch', 'serve')
 ));
-   
---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+  $(function() {
+    $('#stars').barrating({
+      theme: 'fontawesome-stars'
+    });
+  });
+  
+  $(document).ready(function() {
+    $('#content-slider').lightSlider({
+        item: 3,
+        loop:false,
+       speed: 700,
+    
+       pager: true
+
+
+    });
+});
