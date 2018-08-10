94d8ef47b2e9321120111d037437e9030c3bc34c
Rustam4457 <punk60826@gmail.com>
2018-08-10 13:18:54 +0300

    anchor for links and pseudo element in navbar links

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,25 @@
     $('#content-slider').lightSlider({
         item: 3,
         loop:false,
-       speed: 700,
-    
-       pager: true
-
-
+        speed: 700,
+        pager: true
     });
 });
+
+
+///anchor for links
+$(document).ready(function(){
+    $("ul").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
+//responsive menu on phone
+$(document).ready(function(){
+  $('.main-nav-trigger').click(function(){
+    $(this).next('.horizontal-nav').slideToggle()
+  });
+});
+
